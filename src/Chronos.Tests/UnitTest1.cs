bdf5bc9b710b42dc656f411c784edf865bbf8eed
Nathan Smith <nathan.smith@ZIRMED.COM>
2018-02-06 19:19:43 -0500

    lay groundwork for prototype

---
+++ src/Chronos.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chronos.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
