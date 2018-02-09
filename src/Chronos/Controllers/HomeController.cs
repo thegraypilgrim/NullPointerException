79a5e4ff210f2a8c4c4e70972e2e344277953394
nablsi14 <nathaniel.b.sigafoos@gmail.com>
2018-02-08 21:36:07 -0500

    Added basic integration with Google Calendar API

--- src/Chronos/Controllers/HomeController.cs
+++ src/Chronos/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chronos.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,8 @@ namespace Chronos.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            Calendar userCalaneder = new Calendar();
+            return View(userCalaneder);
         }
     }
 }
